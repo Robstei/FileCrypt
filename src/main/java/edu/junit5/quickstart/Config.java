6d73fe1259dc7a1e686b59b7f38b7014b3566653
robin.steil <robin.steil@stud.hs-duesseldorf.de>
2022-05-10 22:09:01 +0200

    created a bad way for writing the config / options, that exist

---
+++ src/main/java/edu/junit5/quickstart/Config.java
@@ -0,0 +1,111 @@
+package edu.junit5.quickstart;
+
+public class Config {
+
+    private static ConfigNode config;
+
+    private static void loadConfig() {
+        String[][][] configArray = {
+                {
+                        {"AES", "AES (Advanced Encryption Standard)"},
+                        {"ECB", "(ECB) Electronic Code Book"},
+                        {"ISO7816-4Padding", "ISO7816-4Padding"}
+                },
+                {
+                        {"AES", "AES (Advanced Encryption Standard)"},
+                        {"ECB", "(ECB) Electronic Code Book"},
+                        {"ISO10126-2Padding", "ISO10126-2Padding"}
+                },
+                {
+                        {"AES", "AES (Advanced Encryption Standard)"},
+                        {"ECB", "(ECB) Electronic Code Book"},
+                        {"PKCS5Padding", "PKCS5Padding/PKCS7Padding"}
+                },
+                {
+                        {"AES", "AES (Advanced Encryption Standard)"},
+                        {"ECB", "(ECB) Electronic Code Book"},
+                        {"TBCPadding", "TBCPadding"}
+                },
+                {
+                        {"AES", "AES (Advanced Encryption Standard)"},
+                        {"ECB", "(ECB) Electronic Code Book"},
+                        {"X9.23Padding", "X9.23Padding"}
+                },
+                {
+                        {"AES", "AES (Advanced Encryption Standard)"},
+                        {"ECB", "(ECB) Electronic Code Book"},
+                        {"ZeroBytePadding", "ZeroBytePadding"}
+                },
+                {
+                        {"AES", "AES (Advanced Encryption Standard)"},
+                        {"CBC", "CBC (Cipher Block Chaining)"},
+                        {"ISO7816-4Padding", "ISO7816-4Padding"}
+                },
+                {
+                        {"AES", "AES (Advanced Encryption Standard)"},
+                        {"CBC", "CBC (Cipher Block Chaining)"},
+                        {"ISO10126-2Padding", "ISO10126-2Padding"}
+                },
+                {
+                        {"AES", "AES (Advanced Encryption Standard)"},
+                        {"CBC", "CBC (Cipher Block Chaining)"},
+                        {"PKCS5Padding", "PKCS5Padding/PKCS7Padding"}
+                },
+                {
+                        {"AES", "AES (Advanced Encryption Standard)"},
+                        {"CBC", "CBC (Cipher Block Chaining)"},
+                        {"TBCPadding", "TBCPadding"}
+                },
+                {
+                        {"AES", "AES (Advanced Encryption Standard)"},
+                        {"CBC", "CBC (Cipher Block Chaining)"},
+                        {"X9.23Padding", "X9.23Padding"}
+                },
+                {
+                        {"AES", "AES (Advanced Encryption Standard)"},
+                        {"CBC", "CBC (Cipher Block Chaining)"},
+                        {"ZeroBytePadding", "ZeroBytePadding"}
+                },
+                {
+                        {"AES", "AES (Advanced Encryption Standard)"},
+                        {"CBC", "CBC (Cipher Block Chaining)"},
+                        {"CTSPadding", "CTSPadding"}
+                },
+                {
+                        {"AES", "AES (Advanced Encryption Standard)"},
+                        {"CTR", "CTR (Counter mode)"},
+                        {"NoPadding", "NoPadding"}
+                },
+                {
+                        {"AES", "AES (Advanced Encryption Standard)"},
+                        {"CFB", "CFB (Cipher FeedBack mode)"},
+                        {"NoPadding", "NoPadding"}
+                },
+                {
+                        {"AES", "AES (Advanced Encryption Standard)"},
+                        {"OFG", "Output Feedback mode"},
+                        {"NoPadding", "NoPadding"}
+                },
+                {
+                        {"ARC4.", "ARC4."}
+                },
+                {
+                        {"ChaCha7539", "ChaCha7539"}
+                }
+        };
+
+        config = new ConfigNode("root", "root", null);
+
+        for (String[][] validConfigCombination : configArray) {
+            config.addValidConfigCombination(validConfigCombination);
+        }
+    }
+
+    public static ConfigNode getConfig() {
+        if (config == null) {
+            loadConfig();
+        }
+        return config;
+    }
+}
+
--- src/main/java/edu/junit5/quickstart/Util.java
+++ src/main/java/edu/junit5/quickstart/Util.java
@@ -9,4 +9,18 @@ public class Util {
         }
         System.out.flush();
     }
+
+    static String getConfig(ConfigNode configNode, int currentLevel) {
+        if (configNode.hasChildren() == false) {
+            return configNode.toString();
+        }
+
+        String values = configNode.toString();
+        int newLevel = currentLevel + 1;
+        for (ConfigNode child : configNode.getChildren()) {
+            values += "-".repeat(newLevel * 2) + " ";
+            values += getConfig(child, newLevel);
+        }
+        return values;
+    }
 }
