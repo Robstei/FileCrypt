f87e9d949c8ccdfbca2de80b0d76ac658b0cfc2d
robin.steil <robin.steil@stud.hs-duesseldorf.de>
2022-07-13 17:36:36 +0200

    testing javafx

--- src/main/java/edu/junit5/quickstart/learning/Test.java
+++ src/main/java/edu/junit5/quickstart/learning/Test.java
@@ -3,21 +3,22 @@ package edu.junit5.quickstart.learning;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.util.encoders.Hex;
 
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
-import java.awt.event.MouseEvent;
-
-import javafx.event.EventHandler;
-
-import java.security.SecureRandom;
-import java.util.Arrays;
-import java.util.EventListener;
+import javax.crypto.Cipher;
 
 public class Test {
     public static void main(String[] args) throws Exception {
+
+        byte[] array = {1};
+        //byte[] decoded = Hex.decode(array);
+        byte[] encoded = Hex.encode(array);
+        String string = Hex.toHexString(array);
 //        EventListener eventListener = new EventListener();
 //        EventHandler eventHandler = new EventHandler();
 //        EventHandler<> eventHandler = new EventHandler();
-
+        Cipher cipher = Cipher.getInstance("AES/OFB/ZeroBytePadding", new BouncyCastleProvider());
+        int a = 1;
+        System.out.println(a);
+        System.out.println(~a);
+        System.out.println(~a + 1);
     }
 }
